7d8211b91a48fdf752ea2a133e450a8884332be8
CollegePaul <psmith@rnngroup.ac.uk>
2021-07-13 16:35:19 +0100

    added dictionary and plantuml diagram

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+#python dictionary
+
+#Lists are accessed by the position in the list, an index
+#Dictionaries are accessed by a key
+
+games = dict([
+    ('minecraft', 'mojang'),
+    ('terraria','Re-Logic'),
+    ('Nintendogs', "Nintendo"),
+    ('Super Mario Bros', 'Nintendo')
+])
+
+print(games)
+print(games['Super Mario Bros'])
+
+games['Pac-Man'] = 'Namco'
+
+del games['Nintendogs']
+
+print(games)
+
+if 'minecraft' in games:
+    print('Yep minecraft is in the dictionary')
+
+print(games.keys())
+
+print(games.values())
