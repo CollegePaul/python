0fe80437dbcc954455bb7b62e35188715ac0b697
CollegePaul <psmith@rnngroup.ac.uk>
2021-07-13 15:00:46 +0100

    added problems

---
+++ ProblemSet/shuffle.py
@@ -0,0 +1,23 @@
+'''
+Shuffle the digits 0-9
+Please Shuffle the digits below, complete the function that will take the list as an input, and return a shuffled list.
+Delete the word 'pass' and put your code there.
+'''
+
+
+def shuffle(digits):
+    pass
+
+
+
+
+
+
+
+
+
+
+'''You don't need to change anything below this line'''
+myList = [0,1,2,3,4,5,6,7,8,9]
+print(shuffle(myList))
+
