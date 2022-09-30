b6cd99d1dae7dfc3c9c04aa579ca1e2024dc833f
CollegePaul <psmith@rnngroup.ac.uk>
2022-09-30 09:26:21 +0100

    updated OOP examples

--- gui/oopGui/2_add_more.py
+++ gui/oopGui/2_add_more.py
@@ -6,21 +6,33 @@ class GUI:
         self.master = master #this is the root object
         master.title("Example OOP") #name of root window
 
-        #add a label
-        self.label = tk.Label(master, text="I am a just label" )
-        self.label.place(x = 100, y=50)
+        self.cpu = Entry(master,"CPU", "Submit", 10,10, self.cpu_method)  #compositon
+        self.gpu = Entry(master,"GPU", "Submit", 10,60)
+        self.ram= Entry(master,"RAM", "Submit", 10,110)
 
-        #add a button
-        self.button = tk.Button(master, text="Run a function (hi)", command=self.hi)
-        self.button.place(x = 200, y=200)
-
-
-    def hi(self):
-        print("Hello!")
+    def cpu_method(self):
+        print("I can do CPU stuff that is special eg " + self.cpu.getEntryValue())
+        
 
 
-class Label():
-    def 
+
+class Entry():
+    def __init__(self, master, caption, buttonCaption, xPos, yPos, method= None):
+        self.caption = caption
+        self.label = tk.Label(master, text=caption , font=("Ariel", 16))
+        self.label.place(x = xPos, y=yPos)
+        self.entry = tk.Entry(master, font=("Ariel", 16))
+        self.entry.place (x =xPos + 60, y = yPos, width = 100)
+        self.button = tk.Button(master, text=buttonCaption, command=method or self.submit)
+        self.button.place(x = xPos + 180, y=yPos)
+
+    def submit(self):
+        value = self.entry.get()
+        print("you have pressed the " + value + " button")
+
+    def getEntryValue(self):
+        return self.entry.get()
+
        
 
 #make sure this is the main program running
